77e4c8ab401b8b456f8f54d15878d2835887e888
sangdo14 <sangdo14@naver.com>
2025-04-08 13:12:08 +0900

    feat: 변수와 상수, 연산자

---
+++ main.js
@@ -0,0 +1,21 @@
+// console.log('hello');
+// document.querySelector('h1').innerText = 'title2';
+// let myAge = 37;
+// const myGender = 'male';
+// console.log(DB);
+
+/*
+  함수(function)
+  : 자주쓰는 구문들을 그룹핑해서 통째로 재활용하기 위한 코드 모음집
+
+  함수 만드는 방법
+  - 선언적 함수 : function 키워드로 선언방식
+  - 대입형 함수 : 변수에 화살표 함수를 대입하는 방식
+*/
+
+let num1 = 3;
+let num2 = 2;
+
+let result = num1 + num2;
+
+console.log(result);
