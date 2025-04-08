cce06865bb575d023a689dfef7bf673c79948846
sangdo14 <sangdo14@naver.com>
2025-04-08 14:39:41 +0900

    feat: 반복문

--- main.js
+++ main.js
@@ -5,67 +5,39 @@
 // console.log(DB);
 
 /*
-함수(function)
-: 자주쓰는 구문들을 그룹핑해서 통째로 재활용하기 위한 코드 모음집
+배열과 반복문
 
-함수 만드는 방법
-- 선언적 함수 : function 키워드로 선언방식 (호출 위치가 자유로움)
-- 대입형 함수 : 변수에 화살표 함수를 대입하는 방식 (선언 이후에만 사용가능)
+배열: 비슷한 성격의 복수 개의 데이터를 그룹핑하기 위한 자료형
+배열의 데이터를 손쉽게 제어하기 위한 문법 반복문
 
-매개변수 (parameter) : 함수 외부에서 특정 값을 내부로 전달해주는 통로명
-인자 (argument) : 매개변수를 통해 실제 전달되는 실질적인 값
+for(let i =0; i<최대수치; i++){
+  반복할 코드
+}
+i값이 0부터 최대수치값 만큼 {}안쪽의 구문을 반복처리
+
+배열의 갯수 구하는 법
+- 배열명.length
+
+배열 반복처리 내장 함수
+- 직접 정의하지 않더라도 자주쓰는 기능들을 묶어서 미리 등록한 함수
+
+배열.forEach(익명함수)
 */
 
-// plus란 이름의 선언적 함수 생성
-// function plus(n1, n2) {
-//   let result = n1 + n2;
-//   console.log(result);
+const colors = ['red', 'green', 'blue', 'purple', 'white', 'black'];
+// console.log(colors.length);
+
+// for (let i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
 // }
 
-// const minus = function (num1, num2) {
-//   console.log(num1 - num2);
-// };
+// colors.forEach((data, index) => {
+//   console.log(data);
+//   console.log(index);
+//   console.log('------------');
+// });
 
-// // es6버전의 정의형 함수 (화살표함수: Arrow function)
-// const divider = (n1, n2) => {
-//   let result = n1 / n2;
-//   console.log(result);
-// };
-
-/*
-  조건문
-: 조건식을 만들어서 조건식이 ture, false인지에 따라서 코드 분기처리하는 식
-
-if(조건식1){
-  조건식1이 참이면 이 코드블록 안쪽의 구문이 실행되면서 조건문 종료
-  조건식1이 거짓이면 다음 조건식2로 넘어감
-}else if(조건식2){
-  조건식2가 참이면 이 코드블록 안쪽의 구문이 실행되면서 조건문 종료
-  조건식2가 거짓이면 다음 조건식2로 넘어감
-}else {
-  만약 위의 조건식이 거짓이면 이곳의 구문을 실행하고 조건문 강제 종료
-}
-*/
-
-const calculator = (n1, n2, how) => {
-  let result;
-  if (how === '+') {
-    result = n1 + n2;
-  } else if (how === '-') {
-    result = n1 - n2;
-  } else if (how === '*') {
-    result = n1 * n2;
-  } else if (how === '/') {
-    result = n1 / n2;
-  } else {
-    result = '산술 기호를 잘못 입력하셨습니다.';
-  }
-
-  console.log(result);
-};
-
-calculator(9, 3, '+');
-calculator(9, 3, '-');
-calculator(9, 3, '*');
-calculator(9, 3, '/');
-calculator(9, 3, '');
+colors.forEach((data, index) => {
+  console.log(data);
+  console.log(index);
+});
